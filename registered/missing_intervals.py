0cb9f865627d17e02052414a47313febc0b6a35e
Paul Swartz <paul@paulswartz.net>
2021-03-22 19:41:26 -0400

    feat: calculate interval distances as HTML
    Using the `routing` module, takes a CSV of the intervals needed to be
    calculated and generates an HTML page with the interval data as well as a map
    of the route.

---
+++ registered/missing_intervals.py
@@ -0,0 +1,325 @@
+"""
+Calculate shortest/fastest paths for missing intervals.
+"""
+import csv
+import re
+import sys
+import attr
+import osmnx as ox
+import requests
+from jinja2 import Template
+from shapely.geometry import Point
+import networkx as nx
+from registered import routing
+
+GARAGE_LOCATIONS = {
+    "fell": Point(-71.08891, 42.42174),
+    "censq": Point(-70.945598, 42.46236),
+    "ncamb": Point(-71.1296316, 42.3973418),
+}
+
+
+def points_from_stop_ids(stop_ids):
+    """
+    Finds Points for the given stop IDs.
+
+    Returns a dictionary mapping the stop ID to the Point.
+    """
+    garages = {
+        garage: point
+        for (garage, point) in GARAGE_LOCATIONS.items()
+        if garage in stop_ids
+    }
+    params = {
+        "filter[id]": ",".join(str(i) for i in stop_ids),
+        "fields[stop]": "latitude,longitude",
+    }
+
+    req = requests.get("https://api-v3.mbta.com/stops/", params=params)
+
+    return {
+        j["id"]: Point(j["attributes"]["longitude"], j["attributes"]["latitude"])
+        for j in req.json()["data"]
+    } | garages
+
+
+@attr.s
+class Stop:  # pylint: disable=too-few-public-methods
+    """
+    A location to calculate an interval (either from or to).
+    """
+
+    id = attr.ib()  # pylint: disable=invalid-name
+    description = attr.ib()
+    point = attr.ib(repr=lambda point: point.wkt)
+
+    _template = Template("{{ this.description }} ({{ this.id }})")
+
+    def render(self):
+        """
+        Render to HTML.
+        """
+        return self._template.render(this=self)
+
+
+@attr.s
+class Interval:
+    """
+    One interval calculation.
+    """
+
+    from_stop = attr.ib()
+    to_stop = attr.ib()
+    interval_type = attr.ib()
+    graph = attr.ib(default=None)
+    fastest_path = attr.ib(default=None)
+    shortest_path = attr.ib(default=None)
+
+    @classmethod
+    def from_stops(cls, from_stop, to_stop, graph, interval_type):
+        """
+        Create an interval given the from/to stops.
+        """
+        if cls.should_ignore(from_stop, to_stop):
+            return cls(from_stop, to_stop, interval_type)
+        ox.utils.log(f"calculating interval from {from_stop} to {to_stop}")
+        try:
+            fastest_path = graph.shortest_path(from_stop.point, to_stop.point)
+            shortest_path = graph.shortest_path(
+                from_stop.point, to_stop.point, weight="length"
+            )
+        except nx.NetworkXNoPath:
+            return cls(from_stop, to_stop, graph, None, None)
+
+        if fastest_path == shortest_path:
+            shortest_path = None
+
+        return cls(
+            from_stop, to_stop, interval_type, graph, fastest_path, shortest_path
+        )
+
+    IGNORE_RE = re.compile(r"\d|Inbound|Outbound")
+
+    @classmethod
+    def should_ignore(cls, from_stop, to_stop):
+        """
+        Return True if we should ignore the given interval.
+
+        - If the descriptions are the same, except for digits (Busway Berth 1 to Busway Berth 2)
+        """
+        return cls.IGNORE_RE.sub("", from_stop.description) == cls.IGNORE_RE.sub(
+            "", to_stop.description
+        )
+
+    _template = Template(
+        """
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>From</th>
+            <th>To</th>
+            <th>Interval Type</th>
+            <th>Google Maps Directions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ this.from_stop.render() }}</td>
+            <td>{{ this.to_stop.render() }}</td>
+            <td>{{ this.interval_type }}</td>
+            <td>
+              <a target="_blank"
+                 href="{{ google_maps_url | e}}">Directions</a></td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Route</th>
+            <th>Length (ft)</th>
+            <th>Compass Direction</th>
+          </tr>
+        </thead>
+        <tbody>
+        {% for item in results %}
+          <tr>
+             {% for cell in item %}<td>{{ cell }}</td>{% endfor %}
+          </tr>
+        {% endfor %}
+        </tbody>
+      </table>
+      {{ folium_map }}
+    </div>
+    """
+    )
+
+    def render(self):
+        """
+        Render to HTML.
+        """
+        google_maps_url = (
+            f"https://www.google.com/maps/dir/?api=1&"
+            f"travelmode=driving&"
+            f"origin={ self.from_stop.point.y },{ self.from_stop.point.x }&"
+            f"destination={ self.to_stop.point.y },{ self.to_stop.point.x }"
+        )
+        results = self._calculate_results()
+        if results == []:
+            folium_map = ""
+        else:
+            folium_map = self.graph.folium_map(
+                self.from_stop.point,
+                self.to_stop.point,
+                [
+                    path
+                    for path in [self.fastest_path, self.shortest_path]
+                    if path is not None
+                ],
+                height=600,
+                width=600,
+            )
+            folium_map = folium_map._repr_html_()  # pylint: disable=protected-access
+
+        return self._template.render(
+            this=self,
+            google_maps_url=google_maps_url,
+            results=results,
+            folium_map=folium_map,
+        )
+
+    def _calculate_results(self):
+        if not self.fastest_path:
+            return []
+        results = [
+            (
+                "Fastest (red)",
+                self.meters_to_feet(self.graph.path_length(self.fastest_path)),
+                self.graph.compass_direction(self.fastest_path),
+            )
+        ]
+        if self.shortest_path:
+            results.append(
+                (
+                    "Shortest (yellow)",
+                    self.meters_to_feet(self.graph.path_length(self.shortest_path)),
+                    self.graph.compass_direction(self.shortest_path),
+                )
+            )
+        return results
+
+    @staticmethod
+    def meters_to_feet(meters):
+        """
+        Convert the given distance in meters to feet.
+        """
+        return int(meters * 3.281)
+
+
+@attr.s
+class Page:
+    """
+    A full HTML page of interval calculations.
+    """
+
+    intervals = attr.ib(default=[])
+
+    def add(self, interval):
+        """
+        Add an interval to the page for future rendering.
+        """
+        self.intervals.append(interval)
+
+    _template = Template(
+        """
+    <!DOCTYPE html>
+    <html>
+    <body>
+      {% for interval in this.intervals %}
+      {% if loop.index > 1 %}<hr>{% endif %}
+      {{ interval.render() }}
+      {% endfor %}
+    </body>
+    </html>
+    """
+    )
+
+    def render(self):
+        """
+        Render to HTML.
+        """
+        ox.utils.log("rendering page...")
+        return self._template.render(this=self)
+
+
+def parse_csv(input_io):
+    """
+    Parse the CSV data in into a Page.
+    """
+    rows = list(csv.DictReader(input_io))
+
+    return parse_rows(rows)
+
+
+def parse_rows(rows):
+    """
+    Parse the given iterable of rows into a Page.
+    """
+    row_count = len(rows)
+    stop_ids = {
+        stop_id
+        for row in rows
+        for stop_id in [row["FromStopNumber"], row["ToStopNumber"]]
+    }
+    stop_locations = points_from_stop_ids(stop_ids)
+    graph = routing.RestrictedGraph.from_points(stop_locations.values())
+
+    page = Page()
+
+    for (index, row) in enumerate(rows, 1):
+        ox.utils.log(f"processing row {index} of {row_count}: {repr(row)}")
+        from_stop = Stop(
+            row["FromStopNumber"],
+            row["FromStopDescription"],
+            stop_locations[row["FromStopNumber"]],
+        )
+        to_stop = Stop(
+            row["ToStopNumber"],
+            row["ToStopDescription"],
+            stop_locations[row["ToStopNumber"]],
+        )
+        interval = Interval.from_stops(from_stop, to_stop, graph, row["IntervalType"])
+        page.add(interval)
+
+    return page
+
+
+def main(argv):
+    """
+    Entrypoint for the Missing Intervals Calculation.
+    """
+    if len(argv) == 1:
+        # stdin -> stdout
+        page = parse_csv(sys.stdin)
+        sys.stdout.write(page.render())
+
+    elif len(argv) == 2:
+        # file -> stdout
+        with open(argv[1]) as input_io:
+            page = parse_csv(input_io)
+            sys.stdout.write(page.render())
+
+    elif len(argv) == 3:
+        with open(argv[1]) as input_io:
+            with open(argv[2], "w") as out_io:
+                ox.config(log_console=True)
+                page = parse_csv(input_io)
+                out_io.write(page.render())
+
+    else:
+        raise RuntimeError("expected 0, 1, or 2 command line arguments")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ tests/test_missing_intervals.py
@@ -0,0 +1,42 @@
+from shapely.geometry import Point
+from registered.missing_intervals import parse_rows, Interval, Stop
+
+
+class TestInterval:
+    def test_should_ignore(self):
+        sullivan_1 = Stop("29001", "Sullivan Station Busway - Berth 1", None)
+        sullivan_2 = Stop("20002", "Sullivan Station Busway - Berth 2", None)
+        fields_corner = Stop("323", "Fields Corner Busway", None)
+        chelsea_inbound = Stop("74630", "Chelsea - Inbound", None)
+        chelsea_outbound = Stop("74631", "Chelsea - Outbound", None)
+
+        assert Interval.should_ignore(sullivan_1, sullivan_1)
+        assert Interval.should_ignore(sullivan_1, sullivan_2)
+        assert Interval.should_ignore(sullivan_1, fields_corner) == False
+        assert Interval.should_ignore(chelsea_inbound, chelsea_outbound)
+
+
+class TestStop:
+    def test_repr(self):
+        stop = Stop("123", "Description", Point(-71, 40))
+        assert (
+            repr(stop)
+            == "Stop(id='123', description='Description', point=POINT (-71 40))"
+        )
+
+
+def test_basic_workflow():
+    rows = [
+        {
+            "issueid": "1",
+            "routeversionid": "138.2",
+            "IntervalType": "--",
+            "FromStopNumber": "5774",
+            "FromStopDescription": "Revere St @ Sagamore St",
+            "ToStopNumber": "15795",
+            "ToStopDescription": "Wonderland Busway",
+            "IntervalDescription": "116-Outbound-116-4",
+        }
+    ]
+    page = parse_rows(rows)
+    page.render()
