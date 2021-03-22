0502c19664618e81bcc9dfc92a311a914fda9ba0
Paul Swartz <paul@paulswartz.net>
2021-03-22 19:40:48 -0400

    feat: backend for calculating interval distances
    The `routing` module provides an interface to calculate the bus driving
    distance between two points.

---
+++ registered/routing.py
@@ -0,0 +1,500 @@
+"""
+Calculate shortest/fastest paths for missing intervals.
+"""
+from itertools import count
+import attr
+import folium
+import osmnx as ox
+import rtree
+import shapely
+from shapely.geometry import Point, LineString, MultiPoint, box
+import networkx as nx
+
+DEFAULT_COLORS = ["red", "yellow", "blue", "green"]
+
+
+class NodesCache:
+    """
+    Cache of the nodes Frame, with some helpful methods for querying/updating.
+    """
+
+    def __init__(self, gdf):
+        self.gdf = gdf
+        self.counter = count(gdf.index.max() + 1)
+        self.index = rtree.index.Index(
+            (t.Index, t.geometry.bounds, None) for t in gdf.itertuples()
+        )
+
+    def new_id(self):
+        """
+        Return a new ID to use for a newly-created node.
+        """
+        return next(self.counter)
+
+    def nearest_point(self, point):
+        """
+        Returns a tuple of (ID, Point) for the closest node to the given Point.
+        """
+        item = next(self.index.nearest(point.bounds, objects=True))
+        point = Point(*item.bbox[:2])
+        return (item.id, point)
+
+    def points(self, ids):
+        """
+        Returns the individual Points for the given node IDs.
+        """
+        return self.gdf.loc[ids, "geometry"]
+
+    def update(self, node):
+        """
+        Updates the cache with the new node.
+        """
+        self.gdf = self.gdf.append(node)
+        self.index.add(node.name, node.geometry.bounds)
+
+
+class EdgesCache:
+    """
+    Cache of the edges GeoDataFrame, with some helpful methods for querying/updating.
+    """
+
+    def __init__(self, gdf):
+        self.gdf = gdf
+        self.counter = count()
+        self.index = rtree.index.Index(
+            (next(self.counter), t.geometry.bounds, t.Index) for t in gdf.itertuples()
+        )
+
+    def nearest_edge(self, point):
+        """
+        Return the nearest edge to the given point.
+
+        If more than one edge is closest, returns the one where the point is
+        on the right (not left) side.
+        """
+        nearest = self.gdf.loc[self.index.nearest(point.bounds, objects="raw")]
+        distances = nearest["geometry"].map(point.distance)
+        nearest["distances"] = distances
+        min_distance = distances.min() + 1e-6
+        within_distance = nearest.loc[distances <= min_distance]
+        if len(within_distance) < 2:
+            # only one closest edge, return it
+            return within_distance.iloc[0]
+
+        # otherwise, find which of the multiple edges has the point on the
+        # right-hand side.
+        def calc_angle(geometry):
+            # might need to be updated if we stare simplifying the graph. in
+            # that case, we'd need to find the bearing at the projection of
+            # point on the given geometry. -ps
+            (tail_x, tail_y) = geometry.coords[0]
+            return ox.bearing.get_bearing((tail_y, tail_x), (point.y, point.x))
+
+        point_bearing = within_distance["geometry"].map(calc_angle)
+        offset = point_bearing - within_distance["bearing"]
+        # offsets >0 are on the right-hand side
+        idx = offset.idxmax()
+        return within_distance.loc[idx]
+
+    def geometry(self, from_node, to_node=None):
+        """
+        Return the geometry for the given from/to edge.
+        """
+        if to_node is None:
+            edge = from_node
+        else:
+            edge = (from_node, to_node, 0)
+        return self.gdf.loc[edge, "geometry"]
+
+    def update(self, gdf):
+        """
+        Update the cache with the new edges (as a GeoDataFrame).
+        """
+        self.gdf = self.gdf.append(gdf)
+        for edge in gdf.itertuples():
+            self.index.insert(next(self.counter), edge.geometry.bounds, edge.Index)
+
+
+@attr.s(repr=False)
+class RestrictedGraph:
+    """
+    Model a OSM street graph with turn restrictions.
+
+    - `graph`: a `nx.MultiDiGraph` representing the primitive graph
+    - `restricted_nodes`: a Set of node IDs which have a turn restriction
+    - `restrictions`: a List of (v, {from_osmids}, {to_osmids}) triples which
+      represent invalid turns
+    """
+
+    graph = attr.ib()
+    restricted_nodes = attr.ib(factory=set)
+    restrictions = attr.ib(factory=list)
+
+    def __attrs_post_init__(self):
+        # pylint: disable=attribute-defined-outside-init
+        (nodes, edges) = ox.utils_graph.graph_to_gdfs(self.graph)
+        self._nodes_cache = NodesCache(nodes)
+        self._edges_cache = EdgesCache(edges)
+        self._created_nodes = {}
+
+    def shortest_path(self, from_point, to_point, weight="travel_time"):
+        """
+        Calculate the shortest path from/to given lat/lon pairs.
+
+        The shortest path is either by travel time (default) or by length (weight="length").
+        """
+        orig = self.closest_node(from_point)
+        dest = self.closest_node(to_point)
+
+        try:
+            (_length, path) = nx.shortest_path_with_turn_restrictions(
+                self.graph, orig, dest, self.restricted, weight=weight
+            )
+        except nx.NetworkXNoPath:
+            return None
+
+        return path
+
+    def compass_direction(self, path):
+        """
+        Return the compass direction the path takes at the end.
+
+        North = 0, East = 90, South = 180, West = 270
+
+        None if the direction is unknown (path is only a single node).
+        """
+        if len(path) < 2:
+            return None
+
+        (second, last) = path[-2:]
+        attrs = self.graph.edges[second, last, 0]
+        return attrs["bearing"]
+
+    def closest_node(self, point):
+        """
+        Return the ID of the closest node to the given Point.
+
+        If there isn't an existing node that's close, find the nearest edges
+        and split them at the given point, returning the new node ID.
+        """
+        if point.wkb in self._created_nodes:
+            return self._created_nodes[point.wkb]
+        ox.utils.log(f"finding closest node to {point.wkt}")
+        (nearest_id, nearest_point) = self._nodes_cache.nearest_point(point)
+
+        same_point_tolerance = 0.0001
+        if nearest_point.distance(point) < same_point_tolerance:
+            existing = nearest_id
+            ox.utils.log(f"node already existed {existing}")
+            self._created_nodes[point.wkb] = existing
+            return existing
+
+        ox.utils.log(f"finding closest edge to {point.wkt}")
+        nearest_edge = self._edges_cache.nearest_edge(point)
+        snapped_point = shapely.ops.nearest_points(nearest_edge.geometry, point)[0]
+
+        ox.utils.log(f"snapping {point.wkt} to {snapped_point.wkt}")
+        (nearest_id, nearest_point) = self._nodes_cache.nearest_point(snapped_point)
+        if snapped_point.distance(nearest_point) < same_point_tolerance:
+            ox.utils.log(f"snapped point already existed {nearest_id}")
+            name = nearest_id
+        else:
+            name = self._nodes_cache.new_id()
+            ox.utils.log(f"creating new node {name}")
+            self.split_edge_at_point(nearest_edge.name, name, snapped_point)
+
+        self._created_nodes[point.wkb] = name
+        return name
+
+    def split_edge_at_point(self, edge, name, point):
+        """
+        Given an edge, the new node ID, and a Point, split the given edge in two at Point.
+        """
+        # 1. create a new node at point
+        # 2. delete the old edge
+        # 3. create two new edges, from head to node, and node to tail
+        ox.utils.log(f"splitting {edge} at {point.wkt}")
+        (head, tail) = edge[:2]
+        edge_attrs = self.graph.edges[edge].copy()
+        ox.utils.log(f"edge OSM ID(s): {edge_attrs['osmid']}")
+        length = edge_attrs.pop("length")
+        del edge_attrs["travel_time"]
+        # simple edges don't have a geometry in the graph, only in the cache
+        edge_attrs.pop("geometry", None)
+        geometry = self._edges_cache.geometry(edge)
+        head_percent = geometry.project(point, normalized=True)
+        [head_geometry, tail_geometry] = cut(geometry, head_percent)
+
+        subgraph = nx.MultiDiGraph(crs=self.graph.graph["crs"])
+        subgraph.add_node(name, y=point.y, x=point.x, geometry=point)
+        subgraph.add_node(head, **self.graph.nodes[head])
+        subgraph.add_node(tail, **self.graph.nodes[tail])
+        subgraph.add_edge(
+            head,
+            name,
+            **edge_attrs,
+            length=length * head_percent,
+            geometry=head_geometry,
+        )
+        subgraph.add_edge(
+            name,
+            tail,
+            **edge_attrs,
+            length=length * (1 - head_percent),
+            geometry=tail_geometry,
+        )
+
+        return self.update(subgraph, name)
+
+    def update(self, subgraph, name):
+        """
+        Updates the current RestrictedGraph with a new node (name) and edges.
+
+        Also updates the caches.
+        """
+
+        subgraph = self.add_graph_features(subgraph)
+
+        (nodes, edges) = ox.utils_graph.graph_to_gdfs(subgraph)
+
+        self.graph.update(subgraph)
+        self._nodes_cache.update(nodes.loc[name])
+        self._edges_cache.update(edges)
+
+        return name
+
+    def path_length(self, path):
+        """
+        Returns the length (in meters) of the given path.
+        """
+        return sum(ox.utils_graph.get_route_edge_attributes(self.graph, path, "length"))
+
+    def folium_map(self, from_point, to_point, paths, **kwargs):
+        """
+        Create a `folium.Map` with the given from/to points, and optionally some paths.
+
+        Returns the map.
+        """
+        route_map = folium.Map(
+            tiles="https://cdn.mbta.com/osm_tiles/{z}/{x}/{y}.png",
+            attr="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors",
+            zoom_start=1,
+            **kwargs,
+        )
+
+        for (path, color) in zip(paths, DEFAULT_COLORS):
+            for (from_node, to_node) in zip(path, path[1:]):
+                locations = [
+                    (row[1], row[0])
+                    for row in self._edges_cache.geometry(from_node, to_node).coords
+                ]
+                folium.PolyLine(locations, weight=2, color=color).add_to(route_map)
+
+        folium.Marker(
+            (from_point.y, from_point.x), icon=folium.Icon(icon="play", color="green")
+        ).add_to(route_map)
+        folium.Marker(
+            (to_point.y, to_point.x), icon=folium.Icon(icon="stop", color="red")
+        ).add_to(route_map)
+
+        [east, north, west, south] = MultiPoint([from_point, to_point]).bounds
+        route_map.fit_bounds([(north, east), (south, west)])
+
+        return route_map
+
+    @classmethod
+    def from_points(cls, points):
+        """
+        Create a RestrictedGraph covering a list of (lat, lon) points.
+
+        The polygon covering all the points is generated by finding the
+        bounding box for the points, then querying the OSM API for that box.
+        """
+        # This Overpass API query gets private access roads which don't block
+        # public service vehicles (PSVs) aka buses.
+        psv = '["highway"]["access"="private"]["psv"!~"no"]'
+        polygon = box(*MultiPoint(list(points)).buffer(0.02).bounds)
+        ox.utils.log("fetching drive_service graph")
+        graph = ox.graph_from_polygon(
+            polygon,
+            network_type="drive_service",
+            retain_all=True,
+            simplify=False,
+            clean_periphery=False,
+            truncate_by_edge=False,
+        )
+        try:
+            ox.utils.log("fetching PSV graph")
+            psv_graph = ox.graph_from_polygon(
+                polygon,
+                custom_filter=psv,
+                retain_all=True,
+                simplify=False,
+                clean_periphery=False,
+                truncate_by_edge=False,
+            )
+        except ox._errors.EmptyOverpassResponse:  # pylint: disable=protected-access
+            pass
+        else:
+            graph.update(psv_graph)
+
+        ox.utils.log("fetching restrictions")
+        (restricted_nodes, restrictions) = cls.restrictions_in_polygon(polygon)
+        # simplification disabled for now; causes a test failure -ps
+        # graph = ox.simplification.simplify_graph(graph)
+        ox.utils.log("adding graph features")
+        graph = cls.add_graph_features(graph)
+
+        return cls(
+            graph=graph, restricted_nodes=restricted_nodes, restrictions=restrictions
+        )
+
+    @staticmethod
+    def add_graph_features(graph):
+        """
+        Update the given graph with important features.
+
+        - speeds
+        - travel times
+        - bearings
+        """
+        # impute speed on all edges missing data
+        graph = ox.add_edge_speeds(
+            graph,
+            hwy_speeds={
+                "motorway": 90,
+                "trunk": 90,
+                "trunk_link": 60,
+                "primary": 60,
+                "secondary": 50,
+                "tertiary": 30,
+                "private": 8,
+                "service": 8,
+                "residential": 0.001,
+            },
+        )
+        # calculate travel time (seconds) for all edges
+        graph = ox.add_edge_travel_times(graph)
+
+        # add edge bearings
+        graph = ox.add_edge_bearings(graph)
+
+        return graph
+
+    # pylint: disable=too-many-arguments
+    def restricted(self, origin, turn, dest, from_attrs, to_attrs):
+        """
+        Return a boolean indicating if the given turn is restricted.
+
+        A turn is restricted if there is a `via` relation of `type`
+        `restriction` and a `restriction` starting with `no_` (like
+        `no_left_turn` or `no_uturn`)
+
+        It is also restricted if the first and last nodes are the same (a
+        U-turn).
+        """
+        if origin == dest:
+            # avoid u-turns
+            return True
+        if turn not in self.restricted_nodes:
+            return False
+        from_ways = self.ensure_set(from_attrs["osmid"])
+        to_ways = self.ensure_set(to_attrs["osmid"])
+
+        for (node, invalid_from, invalid_to) in self.restrictions:
+            if node != turn:
+                continue
+            if (invalid_from & from_ways) and (invalid_to & to_ways):
+                return True
+        return False
+
+    @staticmethod
+    def ensure_set(value):
+        """
+        Given a single value or multiple values, ensure that we return a set.
+        """
+        if isinstance(value, int):
+            return {value}
+
+        return set(value)
+
+    @classmethod
+    def restrictions_in_polygon(cls, polygon):
+        """
+        Fetch the turn restrictions inside the given polygon.
+
+        Uses some internal OSMnx methods.
+        """
+        # pylint: disable=protected-access
+        settings = ox.downloader._make_overpass_settings()
+        restricted_nodes = set()
+        restrictions = []
+        for polygon_str in ox.downloader._make_overpass_polygon_coord_strs(polygon):
+            query = (
+                f"{settings};("
+                f'relation["type"="restriction"]["restriction"~"no_"](poly:"{polygon_str}");'
+                f");out;"
+            )
+            response = ox.downloader.overpass_request(data={"data": query})
+            (new_nodes, new_restrictions) = cls.osm_relations_to_restrictions(response)
+            restricted_nodes |= new_nodes
+            restrictions.extend(new_restrictions)
+        return (restricted_nodes, restrictions)
+
+    @staticmethod
+    def osm_relations_to_restrictions(response):
+        """
+        Turn an OSM relations query into two sets: "via" nodes, and (from, via, to) node triples.
+        """
+        ways = {}
+        nodes = set()
+        restrictions = []
+        for element in response["elements"]:
+            if element["type"] == "way":
+                way_nodes = element["nodes"]
+                ways[element["id"]] = way_nodes
+            elif element["type"] == "relation":
+                members = element["members"]
+                node = [
+                    m["ref"]
+                    for m in members
+                    if m["role"] == "via" and m["type"] == "node"
+                ]
+                if len(node) != 1:
+                    # NB: does not handle "via" where it's a "way" not a "node"
+                    continue
+                node = node[0]
+                from_ways = {
+                    m["ref"]
+                    for m in members
+                    if m["role"] == "from" and m["type"] == "way"
+                }
+                to_ways = {
+                    m["ref"]
+                    for m in members
+                    if m["role"] == "to" and m["type"] == "way"
+                }
+                if not from_ways or not to_ways:
+                    continue
+                nodes.add(node)
+                restrictions.append((node, from_ways, to_ways))
+        return (nodes, restrictions)
+
+
+def cut(line, distance):
+    """
+    Cuts a line in two at a distance from its starting point.
+    """
+    # from https://shapely.readthedocs.io/en/stable/manual.html
+    coords = list(line.coords)
+    for i, coord in enumerate(coords):
+        point_distance = line.project(Point(coord), normalized=True)
+        if point_distance == distance:
+            return [LineString(coords[: i + 1]), LineString(coords[i:])]
+        if point_distance > distance:
+            cut_point = line.interpolate(distance, normalized=True)
+            return [
+                LineString(coords[:i] + [(cut_point.x, cut_point.y)]),
+                LineString([(cut_point.x, cut_point.y)] + coords[i:]),
+            ]
+    raise ValueError(f"unable to cut {line.wkt} at {distance}")
---
+++ tests/test_routing.py
@@ -0,0 +1,159 @@
+from itertools import product
+from registered import routing
+import pytest
+from pytest import approx
+from shapely.geometry import Point
+import networkx as nx
+import osmnx as ox
+
+
+# helpful things for debugging:
+ox.config(log_console=True)  # enable console logging inside OSMnx
+#
+# add the below to a test case to write a map with the given path
+# # graph.folium_map(bc_high_school, federal_st, [path]).save("map.html")
+
+
+def assert_has_path(origin, dest, graph=None, weight="travel_time"):
+    """
+    Create a shortest path between origin and dest, and assert that it has non-zero length.
+    """
+    if graph is None:
+        graph = routing.RestrictedGraph.from_points([origin, dest])
+    path = graph.shortest_path(origin, dest, weight=weight)
+    assert path is not None
+    assert graph.path_length(path) > 0
+    return (graph, path)
+
+
+def test_osm_relations_to_restrictions():
+    response = {
+        "elements": [
+            {"type": "node", "id": 1},
+            {"type": "node", "id": 2},
+            {"type": "node", "id": 3},
+            {"type": "node", "id": 4},
+            {"type": "node", "id": 5},
+            {"type": "way", "id": 6, "nodes": [1, 2, 3]},
+            {"type": "way", "id": 7, "nodes": [2, 4, 5]},
+            {
+                "type": "relation",
+                "id": 8,
+                "members": [
+                    {"type": "way", "ref": 6, "role": "from"},
+                    {"type": "node", "ref": 2, "role": "via"},
+                    {"type": "way", "ref": 7, "role": "to"},
+                ],
+            },
+        ]
+    }
+    (nodes, restrictions) = routing.RestrictedGraph.osm_relations_to_restrictions(
+        response
+    )
+    assert set(nodes) == {2}
+    assert restrictions == [(2, {6}, {7})]
+
+
+def test_osm_relations_to_restrictions_same_way_uturn():
+    response = {
+        "elements": [
+            {"type": "node", "id": 1},
+            {"type": "node", "id": 2},
+            {"type": "node", "id": 3},
+            {"type": "way", "id": 4, "nodes": [1, 2, 3]},
+            {
+                "type": "relation",
+                "id": 5,
+                "members": [
+                    {"type": "way", "ref": 4, "role": "from"},
+                    {"type": "node", "ref": 3, "role": "via"},
+                    {"type": "way", "ref": 4, "role": "to"},
+                ],
+            },
+        ]
+    }
+    (nodes, restrictions) = routing.RestrictedGraph.osm_relations_to_restrictions(
+        response
+    )
+    assert set(nodes) == {3}
+    assert restrictions == [(3, {4}, {4})]
+
+
+def test_no_left_turn():
+    bc_high_school = Point(-71.04149, 42.31760)
+    federal_st = Point(-71.056411, 42.355286)
+    bad_node = 61403875  # left turn from morrisey onto day blvd
+    bad_from_edge = 541034265
+    bad_to_edge = 645350240
+
+    (graph, path) = assert_has_path(bc_high_school, federal_st)
+
+    assert graph.path_length(path) == approx(5732, abs=1)
+    # first and last path entries are psuedo-nodes, snapped from the given
+    # shortest-path points
+    assert path[1] in [5845347051, 7718883175]
+    assert path[-2] in [61340621, 7637468345, 1202432362]
+
+    if bad_node in path:
+        index = path.index(bad_node)
+        previous_node = path[index - 1]
+        next_node = path[index + 1]
+        previous_edge = graph.graph.edges[previous_node, bad_node, 0]
+        next_edge = graph.graph.edges[bad_node, next_node, 0]
+        assert (
+            previous_edge["osmid"] != bad_from_edge
+            and next_edge["osmid"] != bad_to_edge
+        ), f"bad turn, coming from {previous_node} going to {next_node}"
+
+
+def test_compass_direction():
+    origin = Point(-71.03991910663855, 42.33306759993236)
+    dest = Point(-71.03599235273778, 42.335613467448354)
+
+    (graph, path) = assert_has_path(origin, dest)
+
+    assert graph.compass_direction(path) == approx(88.7, abs=0.2)
+
+
+def test_folium_map():
+    origin = Point(-71.03991910663855, 42.33306759993236)
+    dest = Point(-71.03599235273778, 42.335613467448354)
+
+    (graph, path) = assert_has_path(origin, dest)
+    folium_map = graph.folium_map(origin, dest, [path])
+    assert folium_map is not None
+    assert folium_map._repr_html_()
+
+
+def test_multiple_points():
+    origins = [Point(-71.03991910663855, 42.33306759993236), Point(-71.04149, 42.31760)]
+    dests = [
+        Point(-71.056411, 42.355286),
+        Point(-71.03599235273778, 42.335613467448354),
+    ]
+    graph = None
+
+    for (origin, dest) in product(origins, dests):
+        (graph, _path) = assert_has_path(origin, dest, graph=graph)
+
+
+class TestRouting:
+    OD_PAIRS = [
+        ((-71.084983, 42.39193), (-71.078666, 42.386049)),
+        ((-70.99272, 42.418574), (-70.99205, 42.413385)),
+        ((-71.101815, 42.436141), (-71.076315, 42.384167)),
+    ]
+
+    def setup_class(self):
+        points = [Point(p) for points in self.OD_PAIRS for p in points]
+        self.graph = routing.RestrictedGraph.from_points(points)
+
+    @pytest.mark.parametrize("weight", ["length", "travel_time"])
+    @pytest.mark.parametrize("origin,dest", OD_PAIRS)
+    def test_has_path(self, weight, origin, dest):
+        origin_pt = Point(origin)
+        dest_pt = Point(dest)
+        (_graph, path) = assert_has_path(
+            origin_pt, dest_pt, graph=self.graph, weight=weight
+        )
+        print(path)
