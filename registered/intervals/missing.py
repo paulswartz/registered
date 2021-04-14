cf778c912a6e9641e2abf3665f96fb2a1d6999d6
Paul Swartz <paul@paulswartz.net>
2021-04-14 11:08:44 -0400

    refactor: pull SQL query out to a module

--- registered/intervals/missing.py
+++ registered/intervals/missing.py
@@ -6,6 +6,7 @@ import csv
 from pathlib import Path
 import osmnx as ox
 from registered import db
+from registered.intervals import query
 from .page import Page
 from .routing import RestrictedGraph, configure_osmnx
 from .interval import Interval
@@ -16,116 +17,15 @@ def read_database():
     """
     Read the missing intervals from the TransitMaster DB.
     """
-    # 8/20/18 Updated by Jennette Rodemeyer to exclude "and
-    # (gni.compass_direction is not null)", since TM17 sets new deadhead
-    # intervals to null heading by default. */
-    sql = """
-SET NOCOUNT ON;
-
-declare @ttvid numeric(9);
-select @ttvid = max(time_table_version_id) from time_table_version;
-
-declare @distval numeric(6);
-SELECT @distval = 0;  -- Distance in feet to look for.
-
-select
-	1 as issueid,
-	@ttvid + 0.2 as routeversionid,
-    gni.interval_id as IntervalId,
-	0 AS IntervalType,
-	gn1.geo_node_abbr AS FromStopNumber,
-	gn1.geo_node_name AS FromStopDescription,
-	gn1.mdt_latitude/10000000 as FromStopLatitude,
-	gn1.mdt_longitude/10000000 as FromStopLongitude,
-	gn2.geo_node_abbr AS ToStopNumber,
-	gn2.geo_node_name AS ToStopDescription,
-	gn2.mdt_latitude/10000000 as ToStopLatitude,
-	gn2.mdt_longitude/10000000 as ToStopLongitude,
-	min(RTRIM(r.route_abbr) + '-' + RTRIM(rd.route_direction_name) + '-' + RTRIM(p.pattern_abbr)) AS IntervalDescription
-from
-	pattern_geo_interval_xref pgix
-inner join pattern p on
-	pgix.pattern_id = p.pattern_id
-inner join route r on
-	p.route_id = r.route_id
-inner join route_direction rd on
-	p.route_direction_id = rd.route_direction_id
-inner join geo_node_interval gni on
-	pgix.geo_node_interval_id = gni.interval_id
-inner join geo_node gn1 on
-	gni.start_point_id = gn1.geo_node_id
-inner join geo_node gn2 on
-	gni.end_point_id = gn2.geo_node_id
-where
-	pgix.time_table_version_id = @ttvid
-	AND ( gni.distance_between_measured = @distval
-		OR gni.distance_between_measured IS NULL )
-	AND ( gni.distance_between_map = @distval
-		OR gni.distance_between_map IS NULL )
-group by
-	gn1.geo_node_abbr,
-	gn1.geo_node_name,
-	gn1.mdt_latitude,
-	gn1.mdt_longitude,
-	gn2.geo_node_abbr,
-	gn2.geo_node_name,
-	gn2.mdt_latitude,
-	gn2.mdt_longitude
-UNION
-select
-	1 as issueid,
-	@ttvid + 0.2 as routeversionid,
-    gni.interval_id as IntervalId,
-    DH_TYPE as IntervalType,
-	gn1.geo_node_abbr AS FromStopNumber,
-	gn1.geo_node_name AS FromStopDescription,
-	gn1.mdt_latitude/10000000 as FromStopLatitude,
-	gn1.mdt_longitude/10000000 as FromStopLongitude,
-	gn2.geo_node_abbr AS ToStopNumber,
-	gn2.geo_node_name AS ToStopDescription,
-	gn2.mdt_latitude/10000000 as ToStopLatitude,
-	gn2.mdt_longitude/10000000 as ToStopLongitude,
-	MIN(RTRIM(r.route_abbr) + '-' + RTRIM(rd.route_direction_name) + '-' + RTRIM(p.pattern_abbr)) AS IntervalDescription
-from
-	deadheads dh
-inner join pattern p on
-	dh.pattern_id = p.pattern_id
-inner join route r on
-	p.route_id = r.route_id
-inner join route_direction rd on
-	p.route_direction_id = rd.route_direction_id
-inner join geo_node_interval gni on
-	dh.geo_node_interval_id = gni.interval_id
-inner join geo_node gn1 on
-	gni.start_point_id = gn1.geo_node_id
-inner join geo_node gn2 on
-	gni.end_point_id = gn2.geo_node_id
-where
-	dh.time_table_version_id = @ttvid
-	AND ( gni.distance_between_measured = @distval
-		OR gni.distance_between_measured IS NULL )
-	AND ( gni.distance_between_map = @distval
-		OR gni.distance_between_map IS NULL )
-group by
-	dh_type,
-	gn1.geo_node_abbr,
-	gn1.geo_node_name,
-	gn1.mdt_latitude,
-	gn1.mdt_longitude,
-	gn2.geo_node_abbr,
-	gn2.geo_node_name,
-	gn2.mdt_latitude,
-	gn2.mdt_longitude
-order by
-	IntervalType,
-	IntervalDescription;
+    distance = 0
+    where = """
+(gni.distance_between_measured = ?
+  OR gni.distance_between_measured IS NULL)
+AND
+(gni.distance_between_map = ?
+  OR gni.distance_between_map IS NULL)
     """
-    conn = db.conn()
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    sql_headers = [desc[0] for desc in cursor.description]
-    result = cursor.fetchall()
-    return [dict(zip(sql_headers, row)) for row in result]
+    return query.read_database(where, (distance, distance))
 
 
 def parse_rows(rows, include_ignored=False):
---
+++ registered/intervals/query.py
@@ -0,0 +1,151 @@
+"""
+Generate a SQL query for returning intervals.
+"""
+from typing import Optional, Sequence, Any, Union, Tuple, Dict
+from registered import db
+
+Parameters = Sequence[Any]
+
+
+def read_database(
+    where: str, parameters: Optional[Parameters] = None
+) -> Dict[str, Any]:
+    """
+    Read intervals from the TransitMaster DB, given a WHERE query and optionally parameters.
+    """
+    conn = db.conn()
+    cursor = conn.cursor()
+    if parameters is None:
+        cursor.execute(sql(where))
+    else:
+        cursor.execute(*sql(where, parameters))
+
+    sql_headers = [desc[0] for desc in cursor.description]
+    result = cursor.fetchall()
+    return [dict(zip(sql_headers, row)) for row in result]
+
+
+SQL = """
+SET NOCOUNT ON;
+
+declare @ttvid numeric(9);
+select @ttvid = max(time_table_version_id) from time_table_version;
+
+select
+	@ttvid + 0.2 as RouteVersionId,
+    gni.interval_id as IntervalId,
+	0 AS IntervalType,
+	gn1.geo_node_abbr AS FromStopNumber,
+	gn1.geo_node_name AS FromStopDescription,
+	gn1.mdt_latitude/10000000 as FromStopLatitude,
+	gn1.mdt_longitude/10000000 as FromStopLongitude,
+	gn2.geo_node_abbr AS ToStopNumber,
+	gn2.geo_node_name AS ToStopDescription,
+	gn2.mdt_latitude/10000000 as ToStopLatitude,
+	gn2.mdt_longitude/10000000 as ToStopLongitude,
+	min(RTRIM(r.route_abbr) + '-' + RTRIM(rd.route_direction_name) + '-' + RTRIM(p.pattern_abbr)) AS IntervalDescription,
+    gni.DISTANCE_BETWEEN_MAP as DistanceBetweenMap,
+    gni.DISTANCE_BETWEEN_MEASURED as DistanceBetweenMeasured,
+    gni.COMPASS_DIRECTION as CompassDirection,
+    CAST(gni.use_map AS INT) as UseMap
+from
+	pattern_geo_interval_xref pgix
+inner join pattern p on
+	pgix.pattern_id = p.pattern_id
+inner join route r on
+	p.route_id = r.route_id
+inner join route_direction rd on
+	p.route_direction_id = rd.route_direction_id
+inner join geo_node_interval gni on
+	pgix.geo_node_interval_id = gni.interval_id
+inner join geo_node gn1 on
+	gni.start_point_id = gn1.geo_node_id
+inner join geo_node gn2 on
+	gni.end_point_id = gn2.geo_node_id
+where
+	pgix.time_table_version_id = @ttvid
+	AND ({where})
+group by
+    gni.interval_id,
+    gni.distance_between_map,
+    gni.distance_between_measured,
+    gni.compass_direction,
+    gni.use_map,
+	gn1.geo_node_abbr,
+	gn1.geo_node_name,
+	gn1.mdt_latitude,
+	gn1.mdt_longitude,
+	gn2.geo_node_abbr,
+	gn2.geo_node_name,
+	gn2.mdt_latitude,
+	gn2.mdt_longitude
+UNION
+select
+	@ttvid + 0.2 as RouteVersionId,
+    gni.interval_id as IntervalId,
+    dh.DH_TYPE as IntervalType,
+	gn1.geo_node_abbr AS FromStopNumber,
+	gn1.geo_node_name AS FromStopDescription,
+	gn1.mdt_latitude/10000000 as FromStopLatitude,
+	gn1.mdt_longitude/10000000 as FromStopLongitude,
+	gn2.geo_node_abbr AS ToStopNumber,
+	gn2.geo_node_name AS ToStopDescription,
+	gn2.mdt_latitude/10000000 as ToStopLatitude,
+	gn2.mdt_longitude/10000000 as ToStopLongitude,
+	MIN(RTRIM(r.route_abbr) + '-' + RTRIM(rd.route_direction_name) + '-' + RTRIM(p.pattern_abbr)) AS IntervalDescription,
+    gni.DISTANCE_BETWEEN_MAP as DistanceBetweenMap,
+    gni.DISTANCE_BETWEEN_MEASURED as DistanceBetweenMeasured,
+    gni.COMPASS_DIRECTION as CompassDirection,
+    CAST(gni.use_map AS INT) as UseMap
+from
+	deadheads dh
+inner join pattern p on
+	dh.pattern_id = p.pattern_id
+inner join route r on
+	p.route_id = r.route_id
+inner join route_direction rd on
+	p.route_direction_id = rd.route_direction_id
+inner join geo_node_interval gni on
+	dh.geo_node_interval_id = gni.interval_id
+inner join geo_node gn1 on
+	gni.start_point_id = gn1.geo_node_id
+inner join geo_node gn2 on
+	gni.end_point_id = gn2.geo_node_id
+where
+	dh.time_table_version_id = @ttvid
+    AND ({where})
+group by
+    gni.interval_id,
+    gni.distance_between_map,
+    gni.distance_between_measured,
+    gni.compass_direction,
+    gni.use_map,
+	dh.dh_type,
+	gn1.geo_node_abbr,
+	gn1.geo_node_name,
+	gn1.mdt_latitude,
+	gn1.mdt_longitude,
+	gn2.geo_node_abbr,
+	gn2.geo_node_name,
+	gn2.mdt_latitude,
+	gn2.mdt_longitude
+order by
+	IntervalType,
+	IntervalDescription;
+"""
+
+
+def sql(
+    where: str, parameters: Optional[Parameters] = None
+) -> Union[str, Tuple[str, Parameters]]:
+    """
+    Return a SQL query, usable for querying the TransitMaster database.
+
+    If provided, parameters are returned duplicated, to account for the face that the WHERE clause
+    is also duplicated.
+    """
+    sql = SQL.format(where=where)
+    if parameters is None:
+        return sql
+
+    return (sql, parameters + parameters)
