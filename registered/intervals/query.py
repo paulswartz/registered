99cd8c9b03ece208f4c1d5e8ad70fc981d583a49
Paul Swartz <paul@paulswartz.net>
2021-04-19 14:28:37 -0400

    cleanup: run sql-format.com on the SQL

--- registered/intervals/query.py
+++ registered/intervals/query.py
@@ -28,116 +28,111 @@ def read_database(
 SQL = """
 SET NOCOUNT ON;
 
-declare @ttvid numeric(9);
-select @ttvid = max(time_table_version_id) from time_table_version;
+DECLARE @ttvid numeric(9);
+SELECT
+  @ttvid = MAX(time_table_version_id)
+FROM time_table_version;
 
-select
-	@ttvid + 0.2 as RouteVersionId,
-    gni.interval_id as IntervalId,
-	0 AS IntervalType,
-	gn1.geo_node_abbr AS FromStopNumber,
-	gn1.geo_node_name AS FromStopDescription,
-	gn1.mdt_latitude/10000000 as FromStopLatitude,
-	gn1.mdt_longitude/10000000 as FromStopLongitude,
-	gn2.geo_node_abbr AS ToStopNumber,
-	gn2.geo_node_name AS ToStopDescription,
-	gn2.mdt_latitude/10000000 as ToStopLatitude,
-	gn2.mdt_longitude/10000000 as ToStopLongitude,
-	MIN(RTRIM(r.route_abbr)) as Route,
-        MIN(RTRIM(rd.route_direction_name)) as Direction,
-        MIN(RTRIM(p.pattern_abbr)) as Pattern,
-    gni.DISTANCE_BETWEEN_MAP as DistanceBetweenMap,
-    gni.DISTANCE_BETWEEN_MEASURED as DistanceBetweenMeasured,
-    gni.COMPASS_DIRECTION as CompassDirection,
-    CAST(gni.use_map AS INT) as UseMap
-from
-	pattern_geo_interval_xref pgix
-inner join pattern p on
-	pgix.pattern_id = p.pattern_id
-inner join route r on
-	p.route_id = r.route_id
-inner join route_direction rd on
-	p.route_direction_id = rd.route_direction_id
-inner join geo_node_interval gni on
-	pgix.geo_node_interval_id = gni.interval_id
-inner join geo_node gn1 on
-	gni.start_point_id = gn1.geo_node_id
-inner join geo_node gn2 on
-	gni.end_point_id = gn2.geo_node_id
-where
-	pgix.time_table_version_id = @ttvid
-	AND ({where})
-group by
-    gni.interval_id,
-    gni.distance_between_map,
-    gni.distance_between_measured,
-    gni.compass_direction,
-    gni.use_map,
-	gn1.geo_node_abbr,
-	gn1.geo_node_name,
-	gn1.mdt_latitude,
-	gn1.mdt_longitude,
-	gn2.geo_node_abbr,
-	gn2.geo_node_name,
-	gn2.mdt_latitude,
-	gn2.mdt_longitude
+SELECT
+  @ttvid + 0.2 AS RouteVersionId,
+  gni.interval_id AS IntervalId,
+  0 AS IntervalType,
+  gn1.geo_node_abbr AS FromStopNumber,
+  gn1.geo_node_name AS FromStopDescription,
+  gn1.mdt_latitude / 10000000 AS FromStopLatitude,
+  gn1.mdt_longitude / 10000000 AS FromStopLongitude,
+  gn2.geo_node_abbr AS ToStopNumber,
+  gn2.geo_node_name AS ToStopDescription,
+  gn2.mdt_latitude / 10000000 AS ToStopLatitude,
+  gn2.mdt_longitude / 10000000 AS ToStopLongitude,
+  MIN(RTRIM(r.route_abbr)) AS Route,
+  MIN(RTRIM(rd.route_direction_name)) AS Direction,
+  MIN(RTRIM(p.pattern_abbr)) AS Pattern,
+  gni.distance_between_map AS DistanceBetweenMap,
+  gni.distance_between_measured AS DistanceBetweenMeasured,
+  gni.compass_direction AS CompassDirection,
+  CAST(gni.use_map AS int) AS UseMap
+FROM pattern_geo_interval_xref pgix
+INNER JOIN pattern p
+  ON pgix.pattern_id = p.pattern_id
+INNER JOIN route r
+  ON p.route_id = r.route_id
+INNER JOIN route_direction rd
+  ON p.route_direction_id = rd.route_direction_id
+INNER JOIN geo_node_interval gni
+  ON pgix.geo_node_interval_id = gni.interval_id
+INNER JOIN geo_node gn1
+  ON gni.start_point_id = gn1.geo_node_id
+INNER JOIN geo_node gn2
+  ON gni.end_point_id = gn2.geo_node_id
+WHERE pgix.time_table_version_id = @ttvid
+AND ({where})
+GROUP BY gni.interval_id,
+         gni.distance_between_map,
+         gni.distance_between_measured,
+         gni.compass_direction,
+         gni.use_map,
+         gn1.geo_node_abbr,
+         gn1.geo_node_name,
+         gn1.mdt_latitude,
+         gn1.mdt_longitude,
+         gn2.geo_node_abbr,
+         gn2.geo_node_name,
+         gn2.mdt_latitude,
+         gn2.mdt_longitude
 UNION
-select
-	@ttvid + 0.2 as RouteVersionId,
-    gni.interval_id as IntervalId,
-    dh.DH_TYPE as IntervalType,
-	gn1.geo_node_abbr AS FromStopNumber,
-	gn1.geo_node_name AS FromStopDescription,
-	gn1.mdt_latitude/10000000 as FromStopLatitude,
-	gn1.mdt_longitude/10000000 as FromStopLongitude,
-	gn2.geo_node_abbr AS ToStopNumber,
-	gn2.geo_node_name AS ToStopDescription,
-	gn2.mdt_latitude/10000000 as ToStopLatitude,
-	gn2.mdt_longitude/10000000 as ToStopLongitude,
-	MIN(RTRIM(r.route_abbr)) as Route,
-        MIN(RTRIM(rd.route_direction_name)) as Direction,
-        MIN(RTRIM(p.pattern_abbr)) as Pattern,
-    gni.DISTANCE_BETWEEN_MAP as DistanceBetweenMap,
-    gni.DISTANCE_BETWEEN_MEASURED as DistanceBetweenMeasured,
-    gni.COMPASS_DIRECTION as CompassDirection,
-    CAST(gni.use_map AS INT) as UseMap
-from
-	deadheads dh
-inner join pattern p on
-	dh.pattern_id = p.pattern_id
-inner join route r on
-	p.route_id = r.route_id
-inner join route_direction rd on
-	p.route_direction_id = rd.route_direction_id
-inner join geo_node_interval gni on
-	dh.geo_node_interval_id = gni.interval_id
-inner join geo_node gn1 on
-	gni.start_point_id = gn1.geo_node_id
-inner join geo_node gn2 on
-	gni.end_point_id = gn2.geo_node_id
-where
-	dh.time_table_version_id = @ttvid
-    AND ({where})
-group by
-    gni.interval_id,
-    gni.distance_between_map,
-    gni.distance_between_measured,
-    gni.compass_direction,
-    gni.use_map,
-	dh.dh_type,
-	gn1.geo_node_abbr,
-	gn1.geo_node_name,
-	gn1.mdt_latitude,
-	gn1.mdt_longitude,
-	gn2.geo_node_abbr,
-	gn2.geo_node_name,
-	gn2.mdt_latitude,
-	gn2.mdt_longitude
-order by
-	IntervalType,
-	Route,
-    Direction,
-    Pattern;
+SELECT
+  @ttvid + 0.2 AS RouteVersionId,
+  gni.interval_id AS IntervalId,
+  dh.dh_type AS IntervalType,
+  gn1.geo_node_abbr AS FromStopNumber,
+  gn1.geo_node_name AS FromStopDescription,
+  gn1.mdt_latitude / 10000000 AS FromStopLatitude,
+  gn1.mdt_longitude / 10000000 AS FromStopLongitude,
+  gn2.geo_node_abbr AS ToStopNumber,
+  gn2.geo_node_name AS ToStopDescription,
+  gn2.mdt_latitude / 10000000 AS ToStopLatitude,
+  gn2.mdt_longitude / 10000000 AS ToStopLongitude,
+  MIN(RTRIM(r.route_abbr)) AS Route,
+  MIN(RTRIM(rd.route_direction_name)) AS Direction,
+  MIN(RTRIM(p.pattern_abbr)) AS Pattern,
+  gni.distance_between_map AS DistanceBetweenMap,
+  gni.distance_between_measured AS DistanceBetweenMeasured,
+  gni.compass_direction AS CompassDirection,
+  CAST(gni.use_map AS int) AS UseMap
+FROM deadheads dh
+INNER JOIN pattern p
+  ON dh.pattern_id = p.pattern_id
+INNER JOIN route r
+  ON p.route_id = r.route_id
+INNER JOIN route_direction rd
+  ON p.route_direction_id = rd.route_direction_id
+INNER JOIN geo_node_interval gni
+  ON dh.geo_node_interval_id = gni.interval_id
+INNER JOIN geo_node gn1
+  ON gni.start_point_id = gn1.geo_node_id
+INNER JOIN geo_node gn2
+  ON gni.end_point_id = gn2.geo_node_id
+WHERE dh.time_table_version_id = @ttvid
+AND ({where})
+GROUP BY gni.interval_id,
+         gni.distance_between_map,
+         gni.distance_between_measured,
+         gni.compass_direction,
+         gni.use_map,
+         dh.dh_type,
+         gn1.geo_node_abbr,
+         gn1.geo_node_name,
+         gn1.mdt_latitude,
+         gn1.mdt_longitude,
+         gn2.geo_node_abbr,
+         gn2.geo_node_name,
+         gn2.mdt_latitude,
+         gn2.mdt_longitude
+ORDER BY IntervalType,
+Route,
+Direction,
+Pattern;
 """
 
 
