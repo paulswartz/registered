176d662dabb7aa8bd8e221848f37e2d9b99e981b
Paul Swartz <paul@paulswartz.net>
2024-10-23 13:27:46 -0400

    fix(environ): parse `.env` file
    Previously we relied on Pipenv parsing the file and putting it into the
    environment for us. Now that we've switched to Uv, that doesn't happen
    so we need to do it manually.

--- registered/__init__.py
+++ registered/__init__.py
@@ -7,4 +7,15 @@ from registered import environ
 import os
 from registered.friendly_environ import FriendlyEnviron
 
-environ = FriendlyEnviron(os.environ)
+env_file_environ = {}
+try:
+    with open(".env") as f:
+        for line in f:
+            parts = line.split("=", maxsplit=1)
+            if len(parts) == 2:
+                [key, value] = parts
+                env_file_environ[key.strip()] = value.strip()
+except FileNotFoundError:
+    pass
+
+environ = FriendlyEnviron(env_file_environ, os.environ)
--- registered/friendly_environ.py
+++ registered/friendly_environ.py
@@ -8,27 +8,30 @@ class FriendlyEnviron:
     Wrapper around os.environ which raises a more helpful error on missing keys.
     """
 
-    def __init__(self, parent):
-        self._parent = parent
+    def __init__(self, *parents):
+        self._parents = parents
 
     def __getitem__(self, key):
         """
         Item access for environment variables.
         """
         self._validate_key_type(key)
-        try:
-            return self._parent[key]
-        except KeyError:
-            raise KeyError(
-                f"{repr(key)}: In order to add this to your environment, add it to the .env file."
-            ) from None
+        for parent in self._parents:
+            if key in parent:
+                return parent[key]
+        raise KeyError(
+            f"{repr(key)}: In order to add this to your environment, add it to the .env file."
+        ) from None
 
     def get(self, key, missing_val=None):
         """
         Optional access for environment variables.
         """
         self._validate_key_type(key)
-        return self._parent.get(key, missing_val)
+        for parent in self._parents:
+            if key in parent:
+                return parent[key]
+        return missing_val
 
     @staticmethod
     def _validate_key_type(key):
